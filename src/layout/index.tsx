0ab172142d96b90c3f29e73e7fe6b5e2e3fc0acd
chen <731885415@qq,com>
2024-12-09 19:18:44 +0800

    Revert "Revert "1""
    This reverts commit cc1d652d77193f7cacda70b90645e0116729ae07.

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,17 +2,15 @@ import { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Skeleton, message } from "antd";
 import { useCommonstore } from "@/stores";
-import { useNavigate, useOutlet } from "react-router-dom";
+import { useNavigate, useOutlet, Outlet } from "react-router-dom";
 import { toggleCollapsed, togglePhone } from "@/stores/menu";
 import { useLocation } from "react-router-dom";
 import { useCommonStore } from "@/stores";
 import type { AppDispatch } from "@/stores";
 import { STORAGE_AUTHORIZE_KEY } from "@/composables/authorization";
 import { getLocalInfo } from "@/utils/local";
-import { setMenuList, setPermissions, setUserInfo } from "@/stores/user";
 import { debounce } from "lodash";
-import { getUserInfo } from "@/api/user";
-import KeepAlive from "react-activation";
+import { AliveScope, KeepAlive } from "react-activation";
 function Layout() {
   const {
     permissions,
@@ -31,34 +29,6 @@ function Layout() {
   const uri = pathname + search;
   const [isLoading, setLoading] = useState(true);
   const [messageApi, contextHolder] = message.useMessage();
-  /** 获取用户信息和权限 */
-  const fetchUserInfo = useCallback(async () => {
-    try {
-      setLoading(true);
-      const { code, data } = await getUserInfo();
-      if (Number(code) !== 200) return;
-      const { user, permissions, menuList } = data;
-      dispatch(setUserInfo(user));
-      dispatch(setMenuList(menuList || []));
-      dispatch(setPermissions(permissions));
-      navigate("/index");
-    } catch (err) {
-      console.error("获取用户数据失败:", err);
-      setPermissions([]);
-    } finally {
-      setLoading(false);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    // 如果没有token，则返回登录页
-    if (!token) {
-      navigate("/login");
-    } else {
-      navigate("/index");
-    }
-  }, [navigate, token, userId]);
 
   // 监测是否需要刷新
   useEffect(() => {
@@ -86,9 +56,9 @@ function Layout() {
 
   return (
     <div id="layout">
-      <KeepAlive id={uri} name={uri}>
-        {outlet}
-      </KeepAlive>
+      {/* <KeepAlive id={uri} name={uri} enabled={false}> */}
+      <Outlet />
+      {/* </KeepAlive> */}
     </div>
   );
 }
